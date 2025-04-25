4f5d77a702291cd9e8c0880f2b4cab7a3e9a0e22
owenfriend24 <owenwfriend@gmail.com>
2025-04-25 14:02:37 -0500

    write pca searchlight function for testing

---
+++ bin/pca_cluster_model.py
@@ -0,0 +1,34 @@
+import nibabel as nib
+import numpy as np
+import statsmodels.formula.api as smf
+import pandas as pd
+
+
+ref = pd.read_csv("/home1/09123/ofriend/analysis/moshigo_model/pca_sl_meta.csv")
+
+df = ref.copy()
+subject_maps = df['path'] # PCA variance maps
+
+cluster_dir='/scratch/09123/ofriend/moshi/pca_sl/results/cluster_masks'
+cluster_1 = nib.load(f"{cluster_dir}/cluster_001.nii.gz").get_fdata().astype(bool)
+cluster_2 = nib.load(f"{cluster_dir}/cluster_002.nii.gz").get_fdata().astype(bool)
+cluster_3 = nib.load(f"{cluster_dir}/cluster_003.nii.gz").get_fdata().astype(bool)
+
+cluster_index = 1
+for cluster_mask in [cluster_1, cluster_2, cluster_3]:
+    means = []
+    for i, path in enumerate(subject_maps):
+        img = nib.load(path).get_fdata()
+        mean_val = np.mean(img[cluster_mask])
+        means.append(mean_val)
+
+    df[f'cluster_{cluster_index}_mean'] = means
+
+    model = smf.mixedlm(f"cluster_{cluster_index}_mean ~ C(age_group) * C(run)", df, groups=df["subject"])
+    result = model.fit()
+    print(f'RAN MODEL FOR VARIANCE EXPLAINED BY PC1 AND PC2 IN CLUSTER {cluster_index}:')
+    print()
+    print(result.summary())
+    print()
+    cluster_index +=1
+df.to_csv("/home1/09123/ofriend/analysis/moshigo_model/pca_cluster_model_results.csv")
