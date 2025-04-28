b08115b959118d276030165c84a20aad153aaa27
owenfriend24 <owenwfriend@gmail.com>
2025-04-28 09:36:44 -0500

    write pca searchlight function for testing

--- bin/pca_cluster_model.py
+++ bin/pca_cluster_model.py
@@ -10,12 +10,14 @@ df = ref.copy()
 subject_maps = df['path'] # PCA variance maps
 
 cluster_dir='/scratch/09123/ofriend/moshi/pca_sl/results/cluster_masks'
-cluster_1 = nib.load(f"{cluster_dir}/cluster_001.nii.gz").get_fdata().astype(bool)
-cluster_2 = nib.load(f"{cluster_dir}/cluster_002.nii.gz").get_fdata().astype(bool)
-cluster_3 = nib.load(f"{cluster_dir}/cluster_003.nii.gz").get_fdata().astype(bool)
+cluster_1 = nib.load(f"{cluster_dir}/cluster_hip.nii.gz").get_fdata().astype(bool)
+cluster_2 = nib.load(f"{cluster_dir}/cluster_l_ifg.nii.gz").get_fdata().astype(bool)
+cluster_3 = nib.load(f"{cluster_dir}/cluster_r_ifg.nii.gz").get_fdata().astype(bool)
+cluster_4 = nib.load(f"{cluster_dir}/cluster_lpfc.nii.gz").get_fdata().astype(bool)
+cluster_5 = nib.load(f"{cluster_dir}/cluster_postcentral.nii.gz").get_fdata().astype(bool)
 
 cluster_index = 1
-for cluster_mask in [cluster_1, cluster_2, cluster_3]:
+for cluster_mask in [cluster_1, cluster_2, cluster_3, cluster_4, cluster_5]:
     means = []
     for i, path in enumerate(subject_maps):
         img = nib.load(path).get_fdata()
