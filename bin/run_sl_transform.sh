370c9040a5c2688d07c3ecd24cdca58e3c9cfc2f
owenfriend24 <owenwfriend@gmail.com>
2025-04-29 10:30:58 -0500

    write pca searchlight function for testing

--- bin/run_sl_transform.sh
+++ bin/run_sl_transform.sh
@@ -21,13 +21,17 @@ source /home1/09123/ofriend/analysis/temple/profile_rsa
 #echo "ran pca searchlight"
 
 for run in 1 2 3; do
-
-  antsApplyTransforms -d 3 -i /scratch/09123/ofriend/moshi/pca_sl/results/${sub}/${sub}_run-${run}_pca12_varExpl.nii.gz \
-  -o /scratch/09123/ofriend/moshi/pca_sl/results/${sub}/${sub}_run-${run}_pca12_varExpl_MNI_nn.nii.gz \
-  -r /home1/09123/ofriend/analysis/temple/bin/templates/MNI152_T1_1mm_brain.nii.gz \
-  -n NearestNeighbor \
-  -t /corral-repl/utexas/prestonlab/moshiGO1/${sub}/anatomy/antsreg/transforms/brain2MNI_1mm_Warp.nii.gz \
-  -t /corral-repl/utexas/prestonlab/moshiGO1/${sub}/anatomy/antsreg/transforms/brain2MNI_1mm_Affine.txt
-
+  fslmaths /scratch/09123/ofriend/moshi/pca_sl/results/${sub}/${sub}_run-${run}_pca12_varExpl_MNI_nn.nii.gz \
+  -dilM \
+  /scratch/09123/ofriend/moshi/pca_sl/results/${sub}/${sub}_run-${run}_pca12_varExpl_MNI_nn_dilated.nii.gz
 done
-echo "transformed searchlight images to MNI"
+#  antsApplyTransforms -d 3 -i /scratch/09123/ofriend/moshi/pca_sl/results/${sub}/${sub}_run-${run}_pca12_varExpl.nii.gz \
+#  -o /scratch/09123/ofriend/moshi/pca_sl/results/${sub}/${sub}_run-${run}_pca12_varExpl_MNI_nn.nii.gz \
+#  -r /home1/09123/ofriend/analysis/temple/bin/templates/MNI152_T1_1mm_brain.nii.gz \
+#  -n NearestNeighbor \
+#  -t /corral-repl/utexas/prestonlab/moshiGO1/${sub}/anatomy/antsreg/transforms/brain2MNI_1mm_Warp.nii.gz \
+#  -t /corral-repl/utexas/prestonlab/moshiGO1/${sub}/anatomy/antsreg/transforms/brain2MNI_1mm_Affine.txt
+#
+#done
+
+#echo "transformed searchlight images to MNI"
