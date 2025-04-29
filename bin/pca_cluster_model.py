a39a7e8769109deeed6fed7fb47267ea176fac8b
owenfriend24 <owenwfriend@gmail.com>
2025-04-29 10:04:15 -0500

    write pca searchlight function for testing

--- bin/pca_cluster_model.py
+++ bin/pca_cluster_model.py
@@ -9,15 +9,15 @@ ref = pd.read_csv("/home1/09123/ofriend/analysis/moshigo_model/pca_sl_meta.csv")
 df = ref.copy()
 subject_maps = df['path'] # PCA variance maps
 
-cluster_dir='/scratch/09123/ofriend/moshi/pca_sl/results/cluster_masks'
-cluster_1 = nib.load(f"{cluster_dir}/cluster_hip.nii.gz").get_fdata().astype(bool)
-cluster_2 = nib.load(f"{cluster_dir}/cluster_l_ifg.nii.gz").get_fdata().astype(bool)
-cluster_3 = nib.load(f"{cluster_dir}/cluster_r_ifg.nii.gz").get_fdata().astype(bool)
-cluster_4 = nib.load(f"{cluster_dir}/cluster_lpfc.nii.gz").get_fdata().astype(bool)
-cluster_5 = nib.load(f"{cluster_dir}/cluster_postcentral.nii.gz").get_fdata().astype(bool)
+cluster_dir='/scratch/09123/ofriend/moshi/pca_sl/results'
+cluster_1 = nib.load(f"{cluster_dir}/hip_cluster_test.nii.gz").get_fdata().astype(bool)
+# cluster_2 = nib.load(f"{cluster_dir}/cluster_l_ifg.nii.gz").get_fdata().astype(bool)
+# cluster_3 = nib.load(f"{cluster_dir}/cluster_r_ifg.nii.gz").get_fdata().astype(bool)
+# cluster_4 = nib.load(f"{cluster_dir}/cluster_lpfc.nii.gz").get_fdata().astype(bool)
+# cluster_5 = nib.load(f"{cluster_dir}/cluster_postcentral.nii.gz").get_fdata().astype(bool)
 
 cluster_index = 1
-for cluster_mask in [cluster_1, cluster_2, cluster_3, cluster_4, cluster_5]:
+for cluster_mask in [cluster_1]: #, cluster_2, cluster_3, cluster_4, cluster_5]:
     means = []
     for i, path in enumerate(subject_maps):
         img = nib.load(path).get_fdata()
