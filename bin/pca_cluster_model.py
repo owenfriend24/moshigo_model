4c95a8d4cd41770d73578ddd1c33be8c6d8b1038
owenfriend24 <owenwfriend@gmail.com>
2025-04-30 10:40:15 -0500

    updated model to back-project clusters into functional space

--- bin/pca_cluster_model.py
+++ bin/pca_cluster_model.py
@@ -2,26 +2,52 @@ import nibabel as nib
 import numpy as np
 import statsmodels.formula.api as smf
 import pandas as pd
+import subprocess
 
 
 ref = pd.read_csv("/home1/09123/ofriend/analysis/moshigo_model/pca_sl_meta.csv")
 
 df = ref.copy()
-subject_maps = df['path'] # PCA variance maps
+subject_maps = df['func_path'] # PCA variance maps
 
 cluster_dir='/scratch/09123/ofriend/moshi/pca_sl/results'
-cluster_1 = nib.load(f"{cluster_dir}/hip_cluster_test.nii.gz").get_fdata().astype(bool)
+cluster_1_path = f"{cluster_dir}/hip_cluster_test.nii.gz"
+cluster_1 = nib.load(cluster_1_path).get_fdata().astype(bool)
 # cluster_2 = nib.load(f"{cluster_dir}/cluster_l_ifg.nii.gz").get_fdata().astype(bool)
 # cluster_3 = nib.load(f"{cluster_dir}/cluster_r_ifg.nii.gz").get_fdata().astype(bool)
 # cluster_4 = nib.load(f"{cluster_dir}/cluster_lpfc.nii.gz").get_fdata().astype(bool)
 # cluster_5 = nib.load(f"{cluster_dir}/cluster_postcentral.nii.gz").get_fdata().astype(bool)
 
 cluster_index = 1
-for cluster_mask in [cluster_1]: #, cluster_2, cluster_3, cluster_4, cluster_5]:
+for cluster_path in [cluster_1_path]: #, cluster_2, cluster_3, cluster_4, cluster_5]:
     means = []
     for i, path in enumerate(subject_maps):
+        sub = df.loc[i, 'subject']
+        run = df.loc[i, 'run']
+
+        #back project the cluster mask into functional space for each subject
+        warp_path = f'/corral-repl/utexas/prestonlab/moshiGO1/moshiGO_{sub}/anatomy/antsreg/transforms/brain2MNI_1mm_InverseWarp.nii.gz'
+        affine_path = f'/corral-repl/utexas/prestonlab/moshiGO1/moshiGO_{sub}/anatomy/antsreg/transforms/brain2MNI_1mm_Affine.txt'
+        # affine followed by ,1 for inverse
+        out_path = f"{cluster_dir}/moshiGO_{sub}/moshiGO_{sub}_run-{run}_MASK_cluster-{cluster_index}.nii.gz"
+
+        cmd = [
+            'antsApplyTransforms', '-d', '3',
+            '-i', cluster_path,
+            '-o', out_path,
+            '-r', path,
+            '-n', 'NearestNeighbor',
+            '-t', f'[affine_path,1]',
+            '-t', warp_path
+        ]
+        subprocess.run(cmd, check = True)
+
+        # now load the functional image and mask
         img = nib.load(path).get_fdata()
-        masked_vals = img[cluster_mask]
+        mask = nib.load(out_path).get_fdata().astype(bool)
+
+        # mask the data and exclude voxels without observations
+        masked_vals = img[mask]
         masked_vals = masked_vals[masked_vals > 0]
         mean_val = np.nanmean(masked_vals)
         means.append(mean_val)
