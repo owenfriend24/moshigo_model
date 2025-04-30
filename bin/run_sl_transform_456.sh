3dcc186f6ade18efc8d1f2e307c54800262b78cd
owenfriend24 <owenwfriend@gmail.com>
2025-04-30 13:09:06 -0500

    updated model to back-project clusters into functional space

--- bin/run_sl_transform_456.sh
+++ bin/run_sl_transform_456.sh
@@ -33,7 +33,7 @@ echo "ran pca searchlight"
 for run in 4 5 6; do
   echo "transforming run ${run}"
   antsApplyTransforms -d 3 -i /scratch/09123/ofriend/moshi/pca_sl/results/${sub}/${sub}_run-${run}_pca12_varExpl.nii.gz \
-  -o /scratch/09123/ofriend/moshi/pca_sl/results/${sub}/${sub}_run-${run}_pca12_varExpl_MNI_nn_dilated.gz \
+  -o /scratch/09123/ofriend/moshi/pca_sl/results/${sub}/${sub}_run-${run}_pca12_varExpl_MNI_nn_dilated.nii.gz \
   -r /home1/09123/ofriend/analysis/temple/bin/templates/MNI152_T1_1mm_brain.nii.gz \
   -n NearestNeighbor \
   -t /corral-repl/utexas/prestonlab/moshiGO1/${sub}/anatomy/antsreg/transforms/brain2MNI_1mm_Warp.nii.gz \
