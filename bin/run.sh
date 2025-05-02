def59ddac4b9fc6b454b3d6e557a98a07e9e293c
owenfriend24 <owenwfriend@gmail.com>
2025-05-02 15:05:19 -0500

    test plot_ssm_traj

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
 subject=$1
-
-python /home1/09123/ofriend/analysis/moshigo_model/bin/plot_ssm_traj.py
+python /home1/09123/ofriend/analysis/moshigo_model/bin/pca_cluster_model.py
+#python /home1/09123/ofriend/analysis/moshigo_model/bin/plot_ssm_traj.py
