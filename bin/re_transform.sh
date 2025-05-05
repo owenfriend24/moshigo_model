486b6c05fb614bb6e9c4fa5e1d62b8c52af8d08a
owenfriend24 <owenwfriend@gmail.com>
2025-05-05 15:38:14 -0500

    new profile

--- bin/re_transform.sh
+++ bin/re_transform.sh
@@ -6,11 +6,15 @@ if [[ $# -lt 1 ]]; then
     exit 1
 fi
 
-sub=$1
+subject=$1
 
-## create new warp/affine files for T1 to T2 transformation
-ANTS 3 -m CC[/home1/09123/ofriend/analysis/temple/bin/templates/MNI152_T1_1mm_brain.nii.gz, \
-/corral-repl/utexas/prestonlab/moshiGO1/${sub}/anatomy/antsreg/data/funcunwarpspace/brain.nii.gz,1,5] \
--t SyN[0.25] -r Gauss[3,0] -o $SCRATCH/moshi/pca_sl/results/${sub}/test_new_func_to_mni1mm_ \
--i 30x90x20 --use-Histogram-Matching \
---number-of-affine-iterations 10000x10000x10000x10000x10000 --MI-option 32x16000
+python $HOME/analysis/moshigo_model/bin/pca_cluster_model.py
+python $HOME/analysis/moshigo_model/bin/plot_pc_space.py
+python $HOME/analysis/moshigo_model/bin/plot_ssm_traj.py
+
+### create new warp/affine files for T1 to T2 transformation
+#ANTS 3 -m CC[/home1/09123/ofriend/analysis/temple/bin/templates/MNI152_T1_1mm_brain.nii.gz, \
+#/corral-repl/utexas/prestonlab/moshiGO1/${sub}/anatomy/antsreg/data/funcunwarpspace/brain.nii.gz,1,5] \
+#-t SyN[0.25] -r Gauss[3,0] -o $SCRATCH/moshi/pca_sl/results/${sub}/test_new_func_to_mni1mm_ \
+#-i 30x90x20 --use-Histogram-Matching \
+#--number-of-affine-iterations 10000x10000x10000x10000x10000 --MI-option 32x16000
