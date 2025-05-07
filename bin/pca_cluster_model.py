2b6949ecf9e89241017e6acf528c114698fa9087
owenfriend24 <owenwfriend@gmail.com>
2025-05-07 08:52:34 -0500

    new profile

--- bin/pca_cluster_model.py
+++ bin/pca_cluster_model.py
@@ -28,7 +28,8 @@ cluster_3_path = f"{cluster_dir}/ifg_acc_clust.nii.gz.nii.gz"
 # cluster_4 = nib.load(f"{cluster_dir}/cluster_lpfc.nii.gz").get_fdata().astype(bool)
 # cluster_5 = nib.load(f"{cluster_dir}/cluster_postcentral.nii.gz").get_fdata().astype(bool)
 
-cluster_index = 10
+cluster_index = 0
+cluster_names = ['masked_hip', 'unmasked_hip', 'ifg']
 for cluster_path in [cluster_1_path, cluster_2_path, cluster_3_path]:  #, cluster_3_path]: #, cluster_2, cluster_3, cluster_4, cluster_5]:
     means = []
     for i, path in enumerate(subject_maps):
@@ -42,7 +43,7 @@ for cluster_path in [cluster_1_path, cluster_2_path, cluster_3_path]:  #, cluste
             warp_path = f'/corral-repl/utexas/prestonlab/moshiGO1/moshiGO_{sub}/anatomy/antsreg/transforms/brain2MNI_1mm_InverseWarp.nii.gz'
             affine_path = f'/corral-repl/utexas/prestonlab/moshiGO1/moshiGO_{sub}/anatomy/antsreg/transforms/brain2MNI_1mm_Affine.txt'
         # affine followed by ,1 for inverse
-        out_path = f"{cluster_dir}/moshiGO_{sub}/moshiGO_{sub}_run-{run}_MASK_cluster-{cluster_index}.nii.gz"
+        out_path = f"{cluster_dir}/moshiGO_{sub}/moshiGO_{sub}_run-{run}_MASK_cluster-{cluster_names[cluster_index]}.nii.gz"
 
         cmd = [
             'antsApplyTransforms', '-d', '3',
@@ -65,14 +66,14 @@ for cluster_path in [cluster_1_path, cluster_2_path, cluster_3_path]:  #, cluste
         mean_val = np.nanmean(masked_vals)
         means.append(mean_val)
     print("transformed clusters")
-    df[f'cluster_{cluster_index}_mean'] = means
+    df[f'cluster_{cluster_names[cluster_index]}_mean'] = means
     print("running model ...")
-    model = smf.mixedlm(f"cluster_{cluster_index}_mean ~ C(age_group) * run", df, groups=df["subject"])
+    model = smf.mixedlm(f"cluster_{cluster_names[cluster_index]}_mean ~ C(age_group) * run", df, groups=df["subject"])
     result = model.fit()
-    print(f'RAN MODEL FOR VARIANCE EXPLAINED BY PC1 AND PC2 IN CLUSTER {cluster_index}:')
+    print(f'RAN MODEL FOR VARIANCE EXPLAINED BY PC1 AND PC2 IN CLUSTER {cluster_names[cluster_index]}:')
     print()
     print(result.summary())
     print()
-    df.to_csv(f"/home1/09123/ofriend/analysis/moshigo_model/pca_cluster_hip_{cluster_index}_model_results_new.csv")
+    df.to_csv(f"/home1/09123/ofriend/analysis/moshigo_model/pca_cluster_{cluster_names[cluster_index]}_model_results_new.csv")
     cluster_index +=1
 
--- bin/plot_pc_space.py
+++ bin/plot_pc_space.py
@@ -9,7 +9,6 @@ from nilearn.masking import apply_mask
 
 for cluster_name in ['masked_hip_acc_clust', 'unmasked_hip_acc_clust', 'ifg_acc_clust']:
 
-
     expdir = '/corral-repl/utexas/prestonlab/moshiGO1'
     subject_metadata_csv = '/home1/09123/ofriend/analysis/moshigo_model/pca_sl_meta_6run.csv'
     output_plot = f'/home1/09123/ofriend/analysis/moshigo_model/test_pca_plot_{cluster_name}.png'
