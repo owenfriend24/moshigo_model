b1594b89a588aa05c57fbfff2b385a6e7f5272a8
owenfriend24 <owenwfriend@gmail.com>
2025-06-19 17:03:00 -0500

    grid functions

---
+++ bin/moshigo_randomise.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Run randomise to test z-statistic images.
+
+if [[ $# -lt 1 ]]; then
+    echo "randomise_new.sh fmdir"
+    exit 1
+fi
+
+fmdir=$1
+
+randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/group_z.nii.gz \
+-o /scratch/09123/ofriend/moshi/grid_coding/randomise/cont \
+-d /scratch/09123/ofriend/moshi/grid_coding/randomise/design.mat \
+-t /scratch/09123/ofriend/moshi/grid_coding/randomise/design.con \
+-m /home1/09123/ofriend/analysis/moshigo_model/mni_gm_2mm.nii.gz \
+-n 5000 -x --uncorrp
+
+randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/group_z.nii.gz \
+-o /scratch/09123/ofriend/moshi/grid_coding/randomise/one_sample \
+-m /home1/09123/ofriend/analysis/moshigo_model/mni_gm_2mm.nii.gz \
+-1 \
+-n 5000 -x  --uncorrp
