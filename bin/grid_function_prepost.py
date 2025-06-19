f15aa5f2b8b4bc495c985d47613b0f3aefe62bb2
owenfriend24 <owenwfriend@gmail.com>
2025-06-19 12:50:19 -0500

    grid functions

--- bin/grid_function_prepost.py
+++ bin/grid_function_prepost.py
@@ -33,6 +33,7 @@ class grid_function_modulo60(Measure):
 
         sim_mod0 = []
         sim_mod30 = []
+        valid_angles = []
 
         for i in range(n):
             for j in range(i + 1, n):
@@ -48,6 +49,7 @@ class grid_function_modulo60(Measure):
 
                     if is_modulo_match(remainder, 0, self.tolerance):
                         sim_mod0.append(sim)
+                        valid_angles.append(diff)
                         # print(f"match for 0/60 condition")
                         # print()
                         # print()
@@ -85,4 +87,4 @@ class grid_function_modulo60(Measure):
         z_stat_60_ovr_30 = (obsstat - np.mean(randstat)) / np.std(randstat)
         #z_stat_30_ovr_60 = - z_stat_60_ovr_30
 
-        return z_stat_60_ovr_30
+        return valid_angles
