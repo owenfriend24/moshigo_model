c3a52e3dfe3c8ce1c96418d5cd264d0f6e67407e
owenfriend24 <owenwfriend@gmail.com>
2025-06-19 18:09:59 -0500

    grid functions

---
+++ bin/smooth_sl.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Smooth functional data
+
+if [[ $# -lt 1 ]]; then
+    echo ""
+    exit 1
+fi
+
+sl_dir=$1
+
+cd ${sl_dir}/
+
+for sub in moshiGO*; do
+    smooth_susan \
+        "${sub}" \
+         "/home1/09123/ofriend/analysis/moshigo_model/mni_gm_2mm.nii.gz"\
+        4 \
+        "smoothed_${sub}.nii.gz""
+
+    echo "Finished smoothing run ${sub}!"
+
+done
