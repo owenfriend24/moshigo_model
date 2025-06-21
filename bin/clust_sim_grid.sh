bbcd9c42565342ac3dc78b6194a112aa16b7f053
owenfriend24 <owenwfriend@gmail.com>
2025-06-21 18:26:21 -0500

    grid functions

---
+++ bin/clust_sim_grid.sh
@@ -0,0 +1,21 @@
+#!/bin/env bash
+#
+# cluster simulations for preliminary age group RS analyses
+
+if [[ $# -lt 1 ]]; then
+    echo "Usage: clust_sim.sh fmriprep_dir"
+    exit 1
+fi
+
+fmriprep_dir=$1
+
+module load afni
+export OMP_NUM_THREADS=None
+
+mkdir -p /scratch/09123/ofriend/moshi/grid_coding/clust_sim
+cd /scratch/09123/ofriend/moshi/grid_coding/clust_sim
+
+3dClustSim -mask /home1/09123/ofriend/analysis/moshigo_model/mni_gm_2mm.nii.gz -acf 0.35069 4.08259 13.397 -nodec -prefix gm_
+3dClustSim -mask /scratch/09123/ofriend/moshi/grid_coding/mni/mni_masks/func_masks/b_erc.nii.gz -acf 0.35069 4.08259 13.397 -nodec -prefix b_erc_
+3dClustSim -mask /scratch/09123/ofriend/moshi/grid_coding/mni/mni_masks/func_masks/b_Olsen_pmERC.nii.gz -acf 0.35069 4.08259 13.397 -nodec -prefix pm_erc_
+
