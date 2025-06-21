d03bd892ae7f3bbc8bf8b9282a146ede8376fabd
owenfriend24 <owenwfriend@gmail.com>
2025-06-21 10:43:50 -0500

    grid functions

--- bin/moshi_sl_grid_last3.py
+++ bin/moshi_sl_grid_last3.py
@@ -103,8 +103,8 @@ if __name__ == "__main__":
         map2nifti(ds, sl_map_60_ovr_30.samples).to_filename(outfile_60)
 
     subprocess.run(["bash", "/home1/09123/ofriend/analysis/moshigo_model/bin/transform_sl_to_2mm.sh", sbj])
-    subprocess.run(['bash','temple'])
-    subprocess.run(["bash", "/home1/09123/ofriend/analysis/moshigo_model/bin/smooth_sl.sh", f"{expdir}/mni", sbj])
+
+    # subprocess.run(["bash", "/home1/09123/ofriend/analysis/moshigo_model/bin/smooth_sl.sh", f"{expdir}/mni", sbj])
 
 
         #map2nifti(ds, sl_map_30_ovr_60.samples).to_filename(outfile_30)
