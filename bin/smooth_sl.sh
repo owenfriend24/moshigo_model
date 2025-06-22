5e73eb0b0cf9266b8c1dc1500c884aba847a9810
owenfriend24 <owenwfriend@gmail.com>
2025-06-22 13:48:09 -0500

    grid functions

--- bin/smooth_sl.sh
+++ bin/smooth_sl.sh
@@ -13,7 +13,7 @@ sub=$2
 cd ${sl_dir}/
 
 smooth_susan \
-    "${sl_dir}/${sub}_60_ovr_30_mni_2mm_last3.nii.gz" \
+    "${sl_dir}/${sub}_60_ovr_30_mni_2mm_NEW.nii.gz" \
      "/home1/09123/ofriend/analysis/moshigo_model/mni_gm_2mm.nii.gz"\
     4 \
     "${sl_dir}/smoothed_${sub}_last3.nii.gz"
