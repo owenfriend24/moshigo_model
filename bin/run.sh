ee5c79de55fc95fd90b0e91f240ec2384e489d97
owenfriend24 <owenwfriend@gmail.com>
2025-06-22 14:49:03 -0500

    grid functions

--- bin/run.sh
+++ bin/run.sh
@@ -5,6 +5,6 @@ subject=$1
 #python /home1/09123/ofriend/analysis/moshigo_model/bin/plot_ssm_traj.py
 
 
-re_transform.sh ${sub}
-transform_sl_to_2mm.sh ${sub}
-smooth_sl.sh /scratch/09123/ofriend/moshi/grid_coding/mni/new ${sub}
+re_transform.sh ${subject}
+transform_sl_to_2mm.sh ${subject}
+smooth_sl.sh /scratch/09123/ofriend/moshi/grid_coding/mni/new ${subject}
