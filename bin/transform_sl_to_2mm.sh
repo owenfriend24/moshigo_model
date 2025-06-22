5420bfb2693a3b0673689d1faadb702cc5b7fac6
owenfriend24 <owenwfriend@gmail.com>
2025-06-22 13:37:43 -0500

    grid functions

--- bin/transform_sl_to_2mm.sh
+++ bin/transform_sl_to_2mm.sh
@@ -9,29 +9,25 @@ fi
 
 sub=$1
 
-# Handle special cases for certain subjects
-if [[ "$sub" == "moshiGO_250" || "$sub" == "moshiGO_230" || "$sub" == "moshiGO_285" || "$sub" == "moshiGO_334" || "$sub" == "moshiGO_277" || "$sub" == "moshiGO_240" || "$sub" == "moshiGO_247" || "$sub" == "moshiGO_213" || "$sub" == "moshiGO_350" || "$sub" == "moshiGO_323" ]]; then
-    warp_path="/corral-repl/utexas/prestonlab/temple/moshigo/results/${sub}/test_new_func_to_mni1mm_Warp.nii.gz"
-    affine_path="/corral-repl/utexas/prestonlab/temple/moshigo/results/${sub}/test_new_func_to_mni1mm_Affine.txt"
-else
-    warp_path="/corral-repl/utexas/prestonlab/moshiGO1/${sub}/anatomy/antsreg/transforms/brain2MNI_1mm_Warp.nii.gz"
-    affine_path="/corral-repl/utexas/prestonlab/moshiGO1/${sub}/anatomy/antsreg/transforms/brain2MNI_1mm_Affine.txt"
-fi
+
+warp_path="/corral-repl/utexas/prestonlab/temple/moshigo/results/${sub}/NEW_func_to_mni2mm_Warp.nii.gz"
+affine_path="/corral-repl/utexas/prestonlab/temple/moshigo/results/${sub}/NEW_func_to_mni2mm_Affine.txt"
+
 
 antsApplyTransforms -d 3 \
-    -i /scratch/09123/ofriend/moshi/grid_coding/${sub}/grid_data/${sub}_60_ovr_30_b_gray_dilated_last3_z.nii.gz \
-    -o /scratch/09123/ofriend/moshi/grid_coding/mni/${sub}_60_ovr_30_mni_2mm_last3.nii.gz \
+    -i /scratch/09123/ofriend/moshi/grid_coding/${sub}/grid_data/${sub}_60_ovr_30_b_gray_dilated_z.nii.gz \
+    -o /scratch/09123/ofriend/moshi/grid_coding/mni/${sub}_60_ovr_30_mni_2mm_NEW.nii.gz \
     -r /home1/09123/ofriend/analysis/moshigo_model/bin/MNI152_T1_2mm_brain.nii.gz \
     -n NearestNeighbor \
     -t ${warp_path} \
     -t ${affine_path}
 
-antsApplyTransforms -d 3 \
-    -i /scratch/09123/ofriend/moshi/grid_coding/${sub}/grid_data/${sub}_60_ovr_30_LATE_b_gray_dilated_last3_z.nii.gz \
-    -o /scratch/09123/ofriend/moshi/grid_coding/mni/${sub}_60_ovr_30_LATE_mni_2mm_last3.nii.gz \
-    -r /home1/09123/ofriend/analysis/moshigo_model/bin/MNI152_T1_2mm_brain.nii.gz \
-    -n NearestNeighbor \
-    -t ${warp_path} \
-    -t ${affine_path}
+#antsApplyTransforms -d 3 \
+#    -i /scratch/09123/ofriend/moshi/grid_coding/${sub}/grid_data/${sub}_60_ovr_30_LATE_b_gray_dilated_last3_z.nii.gz \
+#    -o /scratch/09123/ofriend/moshi/grid_coding/mni/${sub}_60_ovr_30_LATE_mni_2mm_last3.nii.gz \
+#    -r /home1/09123/ofriend/analysis/moshigo_model/bin/MNI152_T1_2mm_brain.nii.gz \
+#    -n NearestNeighbor \
+#    -t ${warp_path} \
+#    -t ${affine_path}
 
 echo "Transformed searchlight images to MNI space."
