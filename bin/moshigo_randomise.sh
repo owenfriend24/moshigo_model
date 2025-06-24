aafa2b9e5110bed796348dc732c533d01416a5ef
owenfriend24 <owenwfriend@gmail.com>
2025-06-24 11:57:01 -0500

    grid functions

--- bin/moshigo_randomise.sh
+++ bin/moshigo_randomise.sh
@@ -9,6 +9,18 @@ fi
 
 sl_dir=$1
 
+randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/adult_z.nii.gz \
+-o /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/one_sample_adult \
+-m /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/mni_gm_2mm_dil_masked.nii.gz \
+-1 \
+-n 5000 -x  --uncorrp
+
+randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/child_z.nii.gz \
+-o /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/one_sample_child \
+-m /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/mni_gm_2mm_dil_masked.nii.gz \
+-1 \
+-n 5000 -x  --uncorrp
+
 #randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/new_group_z.nii.gz \
 #-o /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/age_increasing \
 #-d /scratch/09123/ofriend/moshi/grid_coding/randomise/new/age_increasing.mat \
@@ -44,12 +56,12 @@ sl_dir=$1
 #-m /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/mni_gm_2mm_dil_masked.nii.gz \
 #-n 5000 -x --uncorrp
 #
-randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/new_group_z.nii.gz \
--o /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/imp_score \
--d /scratch/09123/ofriend/moshi/grid_coding/randomise/new/imp_score.mat \
--t /scratch/09123/ofriend/moshi/grid_coding/randomise/new/imp_score.con \
--m /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/mni_gm_2mm_dil_masked.nii.gz \
--n 5000 -x --uncorrp
+#randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/new_group_z.nii.gz \
+#-o /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/imp_score \
+#-d /scratch/09123/ofriend/moshi/grid_coding/randomise/new/imp_score.mat \
+#-t /scratch/09123/ofriend/moshi/grid_coding/randomise/new/imp_score.con \
+#-m /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/mni_gm_2mm_dil_masked.nii.gz \
+#-n 5000 -x --uncorrp
 #
 #randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/new_group_z.nii.gz \
 #-o /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/one_sample_new \
