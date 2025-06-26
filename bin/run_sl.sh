7b2967af9f89fe87c6b50212239207c46851be5e
owenfriend24 <owenwfriend@gmail.com>
2025-06-26 13:47:11 -0500

    grid functions

--- bin/run_sl.sh
+++ bin/run_sl.sh
@@ -18,5 +18,8 @@ moshiGO_284 moshiGO_285 moshiGO_289 moshiGO_291 moshiGO_292 moshiGO_293 moshiGO_
 moshiGO_304 moshiGO_305 moshiGO_306 moshiGO_308 moshiGO_310 moshiGO_312 moshiGO_313 moshiGO_314 moshiGO_315 moshiGO_316 \
 moshiGO_320 moshiGO_321 moshiGO_322 moshiGO_323 moshiGO_324 moshiGO_327 moshiGO_329 moshiGO_331 moshiGO_333 moshiGO_334 \
 moshiGO_336 moshiGO_339 moshiGO_341 moshiGO_343 moshiGO_345 moshiGO_350 moshiGO_351; do
+  /home1/09123/ofriend/analysis/moshigo_model/bin/prep_grid_data.py ${sub} cone
+  /home1/09123/ofriend/analysis/moshigo_model/bin/moshi_sl_grid.py ${sub} cone gm
+  /home1/09123/ofriend/analysis/moshigo_model/bin/prep_grid_data.py ${sub} mountain
   /home1/09123/ofriend/analysis/moshigo_model/bin/moshi_sl_grid.py ${sub} mountain gm
 done
