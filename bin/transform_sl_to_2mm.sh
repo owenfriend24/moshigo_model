4be6c6c56c999a7736198e6b44448cfa7ad4af76
owenfriend24 <owenwfriend@gmail.com>
2025-06-25 19:11:40 -0500

    grid functions

--- bin/transform_sl_to_2mm.sh
+++ bin/transform_sl_to_2mm.sh
@@ -22,6 +22,16 @@ antsApplyTransforms -d 3 \
     -t ${warp_path} \
     -t ${affine_path}
 
+antsApplyTransforms -d 3 \
+    -i /scratch/09123/ofriend/moshi/grid_coding/${sub}/grid_data/${sub}_60_ovr_30_b_gray_dilated_mountain_z.nii.gz \
+    -o /scratch/09123/ofriend/moshi/grid_coding/mni/cond/${sub}_60_ovr_30_mni_2mm_mountain.nii.gz \
+    -r /home1/09123/ofriend/analysis/moshigo_model/bin/MNI152_T1_2mm_brain.nii.gz \
+    -n NearestNeighbor \
+    -t ${warp_path} \
+    -t ${affine_path}
+
+
+
 #antsApplyTransforms -d 3 \
 #    -i /scratch/09123/ofriend/moshi/grid_coding/${sub}/grid_data/${sub}_60_ovr_30_LATE_b_gray_dilated_last3_z.nii.gz \
 #    -o /scratch/09123/ofriend/moshi/grid_coding/mni/${sub}_60_ovr_30_LATE_mni_2mm_last3.nii.gz \
