4cf26cfa17d628ff104febae42ef075c4f80e93e
owenfriend24 <owenwfriend@gmail.com>
2025-06-27 14:56:54 -0500

    grid functions

--- bin/moshigo_randomise.sh
+++ bin/moshigo_randomise.sh
@@ -7,41 +7,51 @@ if [[ $# -lt 1 ]]; then
     exit 1
 fi
 
-sl_dir=$1
+cond=$1
 
-randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/adult_z.nii.gz \
--o /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/one_sample_adult \
+randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/cond/mountain/smoothed/mountain_group_z.nii.gz \
+-o /scratch/09123/ofriend/moshi/grid_coding/mni/cond/mountain/smoothed/age_increasing \
+-d /scratch/09123/ofriend/moshi/grid_coding/randomise/new/age_increasing.mat \
+-t /scratch/09123/ofriend/moshi/grid_coding/randomise/new/age_increasing.con \
 -m /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/mni_gm_2mm_dil_masked.nii.gz \
--1 \
--n 5000 -x  --uncorrp
+-n 5000 -x --uncorrp
 
-randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/child_z.nii.gz \
--o /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/one_sample_child \
+randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/cond/mountain/smoothed/mountain_group_z.nii.gz \
+-o /scratch/09123/ofriend/moshi/grid_coding/mni/cond/mountain/smoothed/age_decreasing \
+-d /scratch/09123/ofriend/moshi/grid_coding/randomise/new/age_decreasing.mat \
+-t /scratch/09123/ofriend/moshi/grid_coding/randomise/new/age_decreasing.con \
 -m /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/mni_gm_2mm_dil_masked.nii.gz \
--1 \
--n 5000 -x  --uncorrp
+-n 5000 -x --uncorrp
 
-#randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/new_group_z.nii.gz \
-#-o /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/age_increasing \
-#-d /scratch/09123/ofriend/moshi/grid_coding/randomise/new/age_increasing.mat \
-#-t /scratch/09123/ofriend/moshi/grid_coding/randomise/new/age_increasing.con \
-#-m /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/mni_gm_2mm_dil_masked.nii.gz \
-#-n 5000 -x --uncorrp
-#
-#randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/new_group_z.nii.gz \
-#-o /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/age_decreasing \
-#-d /scratch/09123/ofriend/moshi/grid_coding/randomise/new/age_decreasing.mat \
-#-t /scratch/09123/ofriend/moshi/grid_coding/randomise/new/age_decreasing.con \
-#-m /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/mni_gm_2mm_dil_masked.nii.gz \
-#-n 5000 -x --uncorrp
-#
-#randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/new_group_z.nii.gz \
-#-o /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/age_grouped \
-#-d /scratch/09123/ofriend/moshi/grid_coding/randomise/new/age_grouped.mat \
-#-t /scratch/09123/ofriend/moshi/grid_coding/randomise/new/age_grouped.con \
-#-m /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/mni_gm_2mm_dil_masked.nii.gz \
-#-n 5000 -x --uncorrp
-#
+randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/cond/mountain/smoothed/mountain_group_z.nii.gz \
+-o /scratch/09123/ofriend/moshi/grid_coding/mni/cond/mountain/smoothed/age_grouped \
+-d /scratch/09123/ofriend/moshi/grid_coding/randomise/new/age_grouped.mat \
+-t /scratch/09123/ofriend/moshi/grid_coding/randomise/new/age_grouped.con \
+-m /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/mni_gm_2mm_dil_masked.nii.gz \
+-n 5000 -x --uncorrp
+
+randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/cond/cone/smoothed/cone_group_z.nii.gz \
+-o /scratch/09123/ofriend/moshi/grid_coding/mni/cond/cone/smoothed/age_increasing \
+-d /scratch/09123/ofriend/moshi/grid_coding/randomise/new/age_increasing_cone.mat \
+-t /scratch/09123/ofriend/moshi/grid_coding/randomise/new/age_increasing_cone.con \
+-m /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/mni_gm_2mm_dil_masked.nii.gz \
+-n 5000 -x --uncorrp
+
+randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/cond/cone/smoothed/cone_group_z.nii.gz \
+-o /scratch/09123/ofriend/moshi/grid_coding/mni/cond/cone/smoothed/age_decreasing \
+-d /scratch/09123/ofriend/moshi/grid_coding/randomise/new/age_decreasing_cone.mat \
+-t /scratch/09123/ofriend/moshi/grid_coding/randomise/new/age_decreasing_cone.con \
+-m /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/mni_gm_2mm_dil_masked.nii.gz \
+-n 5000 -x --uncorrp
+
+randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/cond/cone/smoothed/cone_group_z.nii.gz \
+-o /scratch/09123/ofriend/moshi/grid_coding/mni/cond/cone/smoothed/age_grouped \
+-d /scratch/09123/ofriend/moshi/grid_coding/randomise/new/age_grouped_cone.mat \
+-t /scratch/09123/ofriend/moshi/grid_coding/randomise/new/age_grouped_cone.con \
+-m /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/mni_gm_2mm_dil_masked.nii.gz \
+-n 5000 -x --uncorrp
+
+
 #randomise -i /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/new_group_z.nii.gz \
 #-o /scratch/09123/ofriend/moshi/grid_coding/mni/new2/smoothed/masked/performance \
 #-d /scratch/09123/ofriend/moshi/grid_coding/randomise/new/performance.mat \
