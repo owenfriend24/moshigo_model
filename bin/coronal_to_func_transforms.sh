2d931b70bb7e3648acef91a6d3d64d923020c2b9
owenfriend24 <owenwfriend@gmail.com>
2025-07-08 16:47:09 -0500

    grid functions

---
+++ bin/coronal_to_func_transforms.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ $# -lt 1 ]]; then
+    echo "Usage: manual_ashs_transforms.sh subject ashs_dir"
+
+    exit 1
+fi
+
+sub=$1
+
+ANTS 3 -m CC[/corral-repl/utexas/prestonlab/moshiGO1/${sub}/anatomy/antsreg/data/funcunwarpspace/brain.nii.gz, \
+/corral-repl/utexas/prestonlab/moshiGO1/${sub}/anatomy/coronal_mean_brain.nii.gz,1,5] \
+-t SyN[0.25] -r Gauss[3,0] -o /corral-repl/utexas/prestonlab/temple/moshigo/results/${sub}/NEW_coronal_to_func_ \
+-i 30x90x20 --use-Histogram-Matching \
+--number-of-affine-iterations 10000x10000x10000x10000x10000 --MI-option 32x16000
