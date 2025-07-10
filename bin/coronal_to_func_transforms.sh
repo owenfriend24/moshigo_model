63ea964ed35b073f4d18bfcc6db105c16e7a5e24
owenfriend24 <owenwfriend@gmail.com>
2025-07-10 14:08:43 -0500

    just pull meta data

--- bin/coronal_to_func_transforms.sh
+++ bin/coronal_to_func_transforms.sh
@@ -8,8 +8,8 @@ fi
 
 sub=$1
 
-ANTS 3 -m CC[/corral-repl/utexas/prestonlab/moshiGO1/${sub}/anatomy/antsreg/data/funcunwarpspace/brain.nii.gz, \
+ANTS 3 -m CC[/corral-repl/utexas/prestonlab/moshiGO1/${sub}/anatomy/brain.nii.gz, \
 /corral-repl/utexas/prestonlab/moshiGO1/${sub}/anatomy/coronal_mean_brain.nii.gz,1,5] \
--t SyN[0.25] -r Gauss[3,0] -o /corral-repl/utexas/prestonlab/temple/moshigo/results/${sub}/NEW_coronal_to_func_ \
+-t SyN[0.25] -r Gauss[3,0] -o /corral-repl/utexas/prestonlab/temple/moshigo/results/${sub}/NEW_coronal_to_anat_ \
 -i 30x90x20 --use-Histogram-Matching \
 --number-of-affine-iterations 10000x10000x10000x10000x10000 --MI-option 32x16000
