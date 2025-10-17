3fa3b0068ef61b79c477fd17c3352dba6df458fe
owenfriend24 <owenwfriend@gmail.com>
2025-10-17 12:29:12 -0500

    new randos

--- bin/smooth_sl.sh
+++ bin/smooth_sl.sh
@@ -31,5 +31,5 @@ smooth_susan \
     "${sl_dir}/new4/${sub}_60_ovr_30_mni_2mm.nii.gz" \
      "/home1/09123/ofriend/analysis/moshigo_model/mni_gm_2mm.nii.gz"\
     4 \
-    "${sl_dir}/new3/smoothed/${sub}_60_ovr_30_mni_2mm.nii.gz"
+    "${sl_dir}/new4/smoothed/${sub}_60_ovr_30_mni_2mm.nii.gz"
 echo "Finished smoothing run ${sub}!"
